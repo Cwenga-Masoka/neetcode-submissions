2d2a9c8e7d3cb0cd662f87950485739ef0a0c7dc
Cwenga Masoka <cwengamasoka61@gmail.com>
2026-07-22 20:02:23 +0200

    Add: minimum-stack - submission-0

---
+++ Data Structures & Algorithms/minimum-stack/submission-0.py
@@ -0,0 +1,26 @@
+class MinStack:
+
+    def __init__(self):
+        self.stack = []
+        
+
+    def push(self, val: int) -> None:
+        self.stack.append(val)
+        
+
+    def pop(self) -> None:
+        self.stack.pop()
+        
+
+    def top(self) -> int:
+        return self.stack[-1]
+        
+
+    def getMin(self) -> int:
+        mini = sys.maxsize
+
+        for i in self.stack:
+            if i<mini:
+                mini = i
+        
+        return mini
